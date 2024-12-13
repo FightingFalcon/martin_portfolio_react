6c242f5f6f34e95afa264473075b53fb1259e5dd
martinsaturnia <45557259+FightingFalcon@users.noreply.github.com>
2024-12-13 10:41:01 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import LogoAnimation from './components/LogoAnimation'
+
+function App() {
+
+  return (
+    <>
+      <LogoAnimation />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/LogoAnimation.tsx
@@ -0,0 +1,263 @@
+import React, { useEffect } from 'react';
+// import anime from 'animejs/lib/anime.es.js';
+import anime from 'animejs';
+
+const LogoAnimation: React.FC = () => {
+  useEffect(() => {
+    const logoEl = document.querySelector('.logo-animation') as HTMLElement | null;
+    const innerWidth = window.innerWidth;
+    const maxWidth = 840;
+    const logoScale = innerWidth <= maxWidth ? innerWidth / maxWidth : 1;
+
+    function explodeParticles() {
+      const krumelur = document.querySelector('.krumelur') as HTMLElement | null;
+      if (!krumelur) return;
+
+      const rect = krumelur.getBoundingClientRect();
+      const numberOfParticles = 20;
+
+      for (let i = 0; i < numberOfParticles; i++) {
+        const particle = document.createElement('div');
+        particle.classList.add('particle');
+        document.body.appendChild(particle);
+
+        const scrollY = window.scrollY || window.pageYOffset;
+        particle.style.left = `${rect.left + window.scrollX}px`;
+        particle.style.top = `${rect.top + scrollY}px`;
+      }
+
+      anime({
+        targets: '.particle',
+        translateX: () => anime.random(-15, 15) + 'vw',
+        translateY: () => anime.random(-15, 15) + 'vh',
+        scale: [1, 0],
+        easing: 'easeOutCirc',
+        duration: 1000,
+        complete: () => {
+          document.querySelectorAll('.particle').forEach(p => p.remove());
+        }
+      });
+    }
+
+    function handleScroll() {
+      anime.remove('.krumelur');
+      anime({
+        targets: '.krumelur',
+        scale: [0.5, 1, 0],
+        easing: 'easeInOutQuad',
+        duration: 500,
+        complete: () => {
+          explodeParticles();
+        }
+      });
+      window.removeEventListener('scroll', handleScroll);
+    }
+
+    function bounceTriangle() {
+      anime({
+        targets: '.triangle',
+        opacity: [0, 1],
+        duration: 4000,
+      });
+      anime({
+        targets: '.triangle',
+        translateY: [0, -10, 0],
+        duration: 800,
+        easing: 'easeInOutQuad',
+        loop: true,
+        delay: 1000,
+      });
+    }
+
+    const logoTimeline = anime.timeline({
+      begin: () => {
+        const lettersEl = document.querySelector('.letters') as HTMLElement | null;
+        if (lettersEl) {
+          lettersEl.style.opacity = '1';
+          lettersEl.style.visibility = 'visible';
+        }
+      },
+      complete: () => {
+        bounceTriangle();
+        window.addEventListener('scroll', handleScroll);
+      }
+    });
+
+    if (logoEl) {
+      logoEl.style.transform = 'translateY(50px) scale(' + logoScale + ')';
+    }
+
+    logoTimeline
+      .add({
+        targets: '.dot-i',
+        translateY: { value: [-250, 0], duration: 1250, elasticity: 400 },
+        rotate: {
+          value: [-90, 0],
+          duration: 500,
+          easing: 'linear'
+        },
+        scale: [
+          { value: [0.5, 1], duration: 1200, easing: 'easeOutQuart' },
+        ],
+        delay: 1200,
+        offset: 0
+      }, '+=750')
+      .add({
+        targets: '.fill.in',
+        strokeDashoffset: {
+          value: [anime.setDashoffset, 0],
+          duration: 600,
+          delay: (el, i) => 700 + (i * 100),
+          easing: 'easeOutQuart',
+        },
+        stroke: {
+          value: ['#FFF', (el) => (el.parentNode as SVGGElement).getAttribute('stroke')!],
+          duration: 500,
+          delay: 500,
+          easing: 'easeInQuad'
+        },
+        opacity: {
+          value: 0,
+          duration: 1,
+          delay: (el, i) => 5500 + (i * 80),
+        },
+        offset: 0
+      }, 250)
+      .add({
+        targets: '.krumelur',
+        opacity: {
+          value: 1,
+        },
+        translateX: {
+          duration: 250,
+          delay: 500,
+          value: 60,
+          easing: 'easeOutQuint',
+        },
+        translateY: {
+          duration: 250,
+          delay: 1000,
+          value: 60,
+          easing: 'easeOutBack'
+        },
+        rotate: {
+          value: 360,
+          delay: 500,
+          duration: 510,
+          easing: 'easeOutQuint',
+        },
+        scale: {
+          value: 0.5,
+          delay: 1000,
+          duration: 250,
+        }
+      }, '-=250')
+      .add({
+        targets: ['.icon-text path', '.icon-text polygon'],
+        opacity: { value: [0, 1], duration: 100, easing: 'linear' },
+        delay: (el, i) => 3000 + (i * 20),
+        offset: 0
+      }, -250)
+      .add({
+        targets: '.letter-e',
+        translateX: {
+          duration: 100,
+          value: -16,
+          easing: 'easeOutQuint'
+        },
+      }, '-=225');
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div style={{ height: '100vh' }} className="logo-container">
+      <div className="logo-animation">
+        <div className="letters" style={{ opacity: 0, visibility: 'hidden' }}>
+          <div className="letter letter-m1">
+            <svg viewBox="0 0 162 162">
+              <g fill="none" fillRule="evenodd" stroke="#1CE2B2">
+                <path className="fill in" strokeWidth="40" d="M 103.91541,161 V 1" />
+                <path className="fill in" strokeWidth="40" d="m 123.91541,20.311186 h -80 L 21.00662,65.005346 V 162" />
+              </g>
+            </svg>
+          </div>
+          <div className="letter letter-m2">
+            <svg viewBox="0 0 162 162">
+              <g fill="none" fillRule="evenodd" stroke="#fff">
+                <path className="fill in" strokeWidth="40" d="M 103.91541,161 V 1" />
+                <path className="fill in" strokeWidth="40" d="m 123.91541,20.311186 h -80 L 21.00662,65.005346 V 162" />
+              </g>
+            </svg>
+          </div>
+          <div className="letter letter-a">
+            <svg viewBox="0 0 162 162">
+              <g fill="none" fillRule="evenodd" stroke="#FB155A">
+                <path className="fill in" strokeWidth="40" d="m 142.13574,162 -0.27434,-142.061874 -97.94599,0.37306 -22.90879,44.69416 0.305665,77.126394 58.721124,-0.30296"/>
+              </g>
+            </svg>
+          </div>
+          <div className="letter letter-r">
+            <svg viewBox="0 0 162 162">
+              <g fill="none" fillRule="evenodd" stroke="#1CE2B2">
+                <path className="fill in" strokeWidth="40" d="m 131.16756,20.383361 h -80 l -22.90879,44.69416 v 96.920219"/>
+              </g>
+            </svg>
+          </div>
+          <div className="letter letter-t">
+            <svg viewBox="0 0 162 162">
+              <g fill="none" fillRule="evenodd" stroke="#fff">
+                <path className="fill in" strokeWidth="40" d="M21 1V161"/>
+                <path className="fill in" strokeWidth="40" d="M81 61h-80"/>
+              </g>
+            </svg>
+          </div>
+          <div className="letter letter-i">
+            <svg viewBox="0 0 82 162">
+              <g fill="none" fillRule="evenodd" stroke="#FB155A">
+                <path className="fill in" strokeWidth="40" d="M 21,161 20.613365,81" style={{display:'inline'}} />
+              </g>
+            </svg>
+          </div>
+          <div className="letter letter-n">
+            <svg viewBox="0 0 162 162">
+              <g fill="none" fillRule="evenodd" stroke="#1CE2B2">
+                <path className="fill in" strokeWidth="40" d="M 142.24771,162 L 141.73228,65.005346 L 118.82349,20.311186 L 20.8775,19.938126 L 20.8775,162"/>
+              </g>
+            </svg>
+          </div>
+          <div className="logo-icon">
+            <div className="icon-text">
+              <svg viewBox="0 0 160 62">
+                <g fill="#FBF3FB" fillRule="evenodd">
+                  <path d="m 102.72424,44.032665 0,-19.632636 4.06592,-5.514692 7.85648,-0.08499 -0.0164,25.14733" style={{display:'inline',fill:'none',stroke:'#fef7fa',strokeWidth:1.58346}} />
+                  <path d="m 114.63024,43.947677 0.0164,-25.14733 c 4.75846,0 7.88367,-0.02769 11.87288,-0.02769 l 0.0169,25.090034" style={{display:'inline',fill:'none',stroke:'#fef7fa',strokeWidth:1.58346}} />
+                  <polygon className="letter-e" points="159.123,18 159.123,19.368 143.427,19.368 143.427,29.664 158.187,29.664 158.187,31.032 143.427,31.032 143.427,42.336 159.303,42.336 159.303,43.704 141.843,43.704 141.843,18 " />
+                </g>
+              </svg>
+            </div>
+          </div>
+          <div className="dot dot-i">
+            <svg viewBox="0 0 42 42">
+              <g fill="none" fillRule="evenodd" stroke="#FB155A"> 
+                <path className="fill in" strokeWidth="40" d="M 21,1 V 40.756563" style={{display:'inline'}} />
+              </g>
+            </svg>
+          </div>   
+          <div className="dot krumelur">
+            <svg viewBox="0 0 42 42">
+              <g fill="#FB155A" fillRule="evenodd" stroke="#FB155A">
+                {/* intentionally blank as per original code */}
+              </g>
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="triangle"></div>
+    </div>
+  );
+};
+
+export default LogoAnimation;
